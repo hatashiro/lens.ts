66c1bf2c0f6632381c3fd3bd21facde380d99ae1
Hyunje Jun <hyunje.jun@linecorp.com>
2018-03-26 17:46:04 +0900

    Use --type-check option for ts-node in tests

--- test/test.sh
+++ test/test.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # unit tests
-ts-node --no-cache test/test.ts || exit 1
+ts-node --no-cache --type-check test/test.ts || exit 1
 
 # compile failing tests
 for f in $(ls test/failing/*.ts)
 do
-    ts-node --no-cache $f > /dev/null 2>&1
+    ts-node --no-cache --type-check $f > /dev/null 2>&1
     if [ "$?" -eq "0" ]; then
         echo "$f should fail"
         exit 1
